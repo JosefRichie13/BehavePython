fd79dc01fa696a968ecebda04ac50c51846baf9a
Joseph Richie <joseph.richie@qumu.com>
2023-07-07 14:21:39 +0530

    Added support for images in the HTML Report

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,5 @@
 # This is the environment.py file. This hosts all the drivers and includes the before and after methods.
+
 import geckodriver_autoinstaller
 from selenium import webdriver
 import chromedriver_autoinstaller
@@ -7,10 +8,9 @@ from features.helpers.pythonmethods import HelperMethods
 from features.helpers.seleniummethods import ResetAppState
 
 
+# This is the before, this method is executed before all the tests, so we are initializing the driver here
+# It uses the auto installer, so no need to specify a driver directly
 def before_all(context):
-    # This is the before, this method is executed before all the tests, so we are initializing the driver here
-    # It uses the auto installer, so no need to specify a driver directly
-
     chromedriver_autoinstaller.install()
     context.driver = webdriver.Chrome()
 
@@ -20,15 +20,28 @@ def before_all(context):
     context.driver.implicitly_wait(10)
 
 
+# This is the after, this method is executed after all the tests, so we are closing the the driver
+# here. If we have any cleanup methods, put it above driver quit method.
 def after_all(context):
-    # This is the after, this method is executed after all the tests, so we are closing the the driver
-    # here. If we have any cleanup methods, put it above driver quit method.
     HelperMethods.ClearTempData()
     context.driver.quit()
 
 
+# This is the before tag method. This will be executed before the specified tag is run
 def before_tag(context, tag):
-    # This is the before tag method. This will be executed before the specified tag is run
     if tag == "cleanappstate":
         ResetAppState(context)
 
+
+# This is the after scenario method. It runs after every scenario, its used to take screenshots and attach to the report
+def after_scenario(context, scenario):
+    def ScreenshotData(mime_type, data, title):
+        non_empty_data = " " if not data else data
+        for formatter in context._runner.formatters:
+            if "html" in formatter.name:
+                formatter.embed(mime_type=mime_type, data=non_empty_data)
+                return
+    context.embed = ScreenshotData
+
+    Screenshot = context.driver.get_screenshot_as_base64()
+    context.embed(mime_type="image/png", data=Screenshot, title="Screenshot")
